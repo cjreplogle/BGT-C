a54d50359c38088fd70eae47682a40222b09695c
CJ Replogle <55760419+cjreplogle@users.noreply.github.com>
2023-06-27 15:04:33 -0400

    Update wlbgt.c

--- wlbgt.c
+++ wlbgt.c
@@ -8,19 +8,19 @@ int main(void){
         int state=0;
         while(state==0){
                 printf("\e[1;1H\e[2J");
-                printf("/-------------------------------------------------------\\\n");
-                printf("|--------- Whitlock Lab Bioprinting GCode Tool ---------|\n");
-                printf("|---------- v0.1, made by CJ Replogle c. 2023 ----------|\n");
-                printf("|-------------------------------------------------------|\n");
-                printf("|                                                       |\n");
-                printf("| 1: Generate New Scaffold                              |\n");
-                printf("| 2: Use Preset Infill Pattern                          |\n");
-                printf("| 3: Print Variable Test (Not Working)                  |\n");
-                printf("| 4: Help                                               |\n");
-                printf("| 5: About this Program                                 |\n");
-                printf("| 6: Exit                                               |\n");
-                printf("|                                                       |\n");
-                printf("\\------------------------------------------------------/\n\n-> ");
+                printf("/------------------------------------------------------\\\n");
+                printf("|--------------- Bioprinting GCode Tool ---------------|\n");
+                printf("|---------- v0.1 made by CJ Replogle c. 2023 ----------|\n");
+                printf("|------------------------------------------------------|\n");
+                printf("|                                                      |\n");
+                printf("| 1: Generate New Scaffold                             |\n");
+                printf("| 2: Use Preset Infill Pattern                         |\n");
+                printf("| 3: Print Variable Test (Not Working)                 |\n");
+                printf("| 4: Help                                              |\n");
+                printf("| 5: About this Program                                |\n");
+                printf("| 6: Exit                                              |\n");
+                printf("|                                                      |\n");
+                printf("\\-----------------------------------------------------/\n\n-> ");
                 scanf("%i",&state);
 
                 switch (state) {
@@ -28,9 +28,9 @@ int main(void){
                                 infill();
                                 tesselator();
                                 printf("\e[1;1H\e[2J");
-                                printf("/-------------------------------------------------------\\\n");
-                                printf("| Scaffold successfully generated!                      |\n");
-                                printf("\\-------------------------------------------------------/\n\n");
+                                printf("/------------------------------------------------------\\\n");
+                                printf("| Scaffold successfully generated!                     |\n");
+                                printf("\\------------------------------------------------------/\n\n");
                                 printf("Press enter to return to the menu -> ");
                                 scanf("%d",&state);
                                 printf("\e[1;1H\e[2J");
@@ -39,9 +39,9 @@ int main(void){
                         case 2: 
                                 tesselator();
                                 printf("\e[1;1H\e[2J");
-                                printf("/-------------------------------------------------------\\\n");
-                                printf("| Scaffold successfully generated!                      |\n");
-                                printf("\\-------------------------------------------------------/\n\n");
+                                printf("/------------------------------------------------------\\\n");
+                                printf("| Scaffold successfully generated!                     |\n");
+                                printf("\\------------------------------------------------------/\n\n");
                                 printf("Press enter to return to the menu -> ");
                                 scanf("%d",&state);
                                 printf("\e[1;1H\e[2J");
@@ -53,9 +53,9 @@ int main(void){
                                 break;
                         case 4:
                                 printf("\e[1;1H\e[2J");
-                                printf("/-------------------------------------------------------\\\n");
-                                printf("| Haven't wrote this out yet sorry. Good luck.          |\n");
-                                printf("\\-------------------------------------------------------/\n\n");
+                                printf("/------------------------------------------------------\\\n");
+                                printf("| Haven't wrote this out yet sorry. Good luck.         |\n");
+                                printf("\\------------------------------------------------------/\n\n");
                                 printf("Press enter to return to the menu -> ");
                                 scanf("%d",&state);
                                 printf("\e[1;1H\e[2J");
@@ -63,8 +63,8 @@ int main(void){
                                 break;
                         case 5:
                                 printf("\e[1;1H\e[2J");
-                                printf("/-------------------------------------------------------\\\n");
-                                printf("\nHello, and welcome to WLSGT (Whitlock Lab Scaffold GCode Tool). This software was developed by CJ Replogle (a student-researcher) at Cincinnati Children's as a method of generating gcode files for the Whitlock lab for use in bioprinting. ");
+                                printf("/------------------------------------------------------\\\n");
+                                printf("\nHello, and welcome to BGT (Bioprinting GCode Tool). This software was developed by CJ Replogle (a student-researcher) at Cincinnati Children's as a method of generating gcode files for the Whitlock lab for use in bioprinting. ");
                                 printf("Existing software did not have good control over different parameters involving printhead movement and the built-in ability. This software in particular is designed for use with the Cellink BioX 3D printer, although created gcodes might work fine with others.");
                                 printf("to rapidly create gcodes to test things like scaffold porosity and infill structure so I decided to make a program to do it myself.\n\n");
                                 printf("Some things to consider when using this. Firstly, I am a biomedical engineer, not a programmer. This software is developed in-house and probably (definitely) is not perfect. It should get the job done for testing purposes and potentially small scale clinical applications. ");
@@ -81,7 +81,7 @@ int main(void){
                                 printf("to lattice-based counterparts due to microstructural limitations of thermal cross-linking of the bioink.\n\n");
                                 printf("With that word salad out of the way, if there are any issues encountered with this software, reach out to me at ");
                                 printf("replogle.17@buckeyemail.osu.edu (should work until ~2026) and tell me my code is bad. (I already know)\n\n");
-                                printf("\\------------------------------------------------------/\n\n");
+                                printf("\\-----------------------------------------------------/\n\n");
                                 printf("Press enter to return to the menu -> ");
                                 scanf("%d",&state);
                                 printf("\e[1;1H\e[2J");
